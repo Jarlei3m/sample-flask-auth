39b64972416d28a09c32e0e5394080ff76548623
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2024-06-08 16:13:36 -0300

    feat: encrypting password

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import bcrypt
 from flask import Flask, jsonify, request
 from models.user import User
 from database import db
@@ -24,9 +25,13 @@ def login():
   username = data["username"]
   password = data["password"]
 
+  # hashed_password = bcrypt.checkpw(password,)
+
   if username and password:
     user = User.query.filter_by(username=username).first()
-    if user and user.password == password:
+    hashed = bcrypt.checkpw(str.encode(password), str.encode(user.password))
+
+    if user and hashed:
       login_user(user)
       return jsonify({"message": "Autenticação realizada com sucesso!"})
   
@@ -45,7 +50,8 @@ def create_user():
   password = data.get("password")
 
   if username and password:
-    user = User(username=username, password=password, role='user')
+    hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+    user = User(username=username, password=hashed_password, role='user')
     db.session.add(user)
     db.session.commit()
     return jsonify({"message": "Usuário cadastrado com sucesso!"})
